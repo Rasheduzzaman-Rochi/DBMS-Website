2d8178126d96429fdca89950b030c51c515ea2df
SuzanaSohorab <suzanasohorab5@gmail.com>
2025-04-09 16:27:57 +0600

    register form

---
+++ jsFile/dropDown.js
@@ -0,0 +1,19 @@
+// Get the element where the selected designation is displayed
+const dropdown = document.querySelector('#designation'); 
+
+// Get the next sibling element (the list of options) of the dropdown
+const optionsList = dropdown.nextElementSibling; // This gets the <ul> that contains the options
+
+// Add an event listener to the dropdown to toggle the visibility of the options list
+dropdown.addEventListener('click', () => {
+  optionsList.classList.toggle('hidden'); // When clicked, we show or hide the options
+});
+
+// Add an event listener to the options list to update the text when an option is selected
+optionsList.addEventListener('click', (e) => {
+  // Get the text of the clicked option and set it as the new text of the dropdown
+  dropdown.innerHTML = `<span>${e.target.innerText}</span>`; 
+  
+  // Hide the options list after an option is selected
+  optionsList.classList.add('hidden'); 
+});
